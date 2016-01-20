9dc9328670e601e6b32ee1f9cb709450c3a6b801
Elise Harper <eharper4@mail.valenciacollege.edu>
2016-01-19 21:05:47 -0500

    Week 2 Hello World Submission

--- helloworld.py
+++ helloworld.py
@@ -9,18 +9,29 @@
 # make sure that your code contains comments explaining your logic!
 
 print('Hello Friend!')
-print('Please choose a language')
+# Greets user in English
+print('Please choose a language by typing a number')
 print('1. Spanish')
 print('2. Italian')
 print('3. French')
-# I put the print commands on separate lines for formatting purposes. Not sure if there's an easier way to do that.
+# I put the print commands on separate lines for formatting purposes. 
 myLanguage = input()
-# I assigned whatever the user types in to myLanguage with the 3 different outcomes
-if myLanguage == str('Spanish'):
+# I assigned whatever the user types in to myLanguage 
+if myLanguage == '1':
         print('Hola! Como estas?')
-if myLanguage == str('Italian'):
+#This message is displayed in Spanish if the user entered 1
+elif myLanguage == '2':
         print('Ciao, come stai')
-if myLanguage == str('French'):
+#This message is displayed in Italian if the user entered 2
+elif myLanguage == '3':
         print('Salut comment allez-vous')
+#This message is displayed in French if the user entered 3
+
+else:
+        print('Have a nice day!')
+#If the user puts something other than 1, 2 or 3 then it will write this
+exit()
+#exits the program
+
 
 
